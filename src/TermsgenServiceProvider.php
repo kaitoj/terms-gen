cfa26541e3959b9d65aa03891f41f98a5730fb14
Kaito Jacobson <kaito.jacobson@gmail.com>
2020-06-23 13:25:57 +0100

    service provider

---
+++ src/TermsgenServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Kaitoj\Termsgen;
+
+use Illuminate\Support\ServiceProvider;
+
+class TermsgenServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
